cac0dd1826c414d1d28dd63515c8f901289ba71b
GarcelCode <92178583+GarcelCode@users.noreply.github.com>
2022-12-27 19:34:30 -0600

    [LESSON2] Assets, Metadata, and CSS

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,19 @@
-import Link from 'next/link'
+import Head from 'next/head'
+import Layout from '../components/layout'
+import utilStyles from '../styles/utils.module.css'
 
 export default function Home() {
   return (
-    <>
-      <h1>
-        Ir a posts <Link href="/posts/first-post">Posts</Link>
-      </h1>
-    </>
+    <Layout home>
+      <Head>
+        <title>My first app</title>
+      </Head>
+      <section className={utilStyles.headingMd}>
+        <p>
+          Hola mi nombre es Samuel García y este proyecto es mi primer
+          aplicación para NextJS. La fecha de creación fue 27/12/2022
+        </p>
+      </section>
+    </Layout>
   )
 }
