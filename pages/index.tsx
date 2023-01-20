cf68488ae7b37c122847b8a806a6cdb2d8a62b6f
GarcelCode <92178583+GarcelCode@users.noreply.github.com>
2023-01-19 20:16:27 -0600

    [LESSON3] Pre-rendering and Data Fetching

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,29 @@
 import Head from 'next/head'
 import Layout from '../components/layout'
 import utilStyles from '../styles/utils.module.css'
+import { getSortedPostsData } from '../lib/posts'
 
-export default function Home() {
+type singlePostType = {
+  id: string
+  date: string
+  title: string
+  data: string
+}
+
+type homeType = {
+  allPostsData: singlePostType[]
+}
+
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData()
+  return {
+    props: {
+      allPostsData,
+    },
+  }
+}
+
+export default function Home({ allPostsData }: homeType) {
   return (
     <Layout home>
       <Head>
@@ -13,6 +34,16 @@ export default function Home() {
           Hola mi nombre es Samuel García y este proyecto es mi primer
           aplicación para NextJS. La fecha de creación fue 27/12/2022
         </p>
+      </section>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              {title} <br /> {id} <br /> {date}{' '}
+            </li>
+          ))}
+        </ul>
       </section>
     </Layout>
   )
